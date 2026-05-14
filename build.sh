9062c8e6684f929deec451377eea6ecf23752d3c
Soro Franck Albert <sorofranck3@gmail.com>
2026-05-14 17:13:29 +0000

    fix: vercel 006

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-# Arrêter le script si une commande échoue
-set -e
 
-# Afficher chaque commande avant de l'exécuter
+set -e
 set -x
 
-# Installation des dépendances
 echo "Installation des dépendances"
 python3.12 -m pip install -r requirements.txt --break-system-packages
 
-# Collecte des fichiers statiques
+echo "Build Tailwind"
+python3.12 src/manage.py tailwind build
+
 echo "Collection des fichiers statiques"
 python3.12 src/manage.py collectstatic --noinput
