8ebf81105da902e582e5897176ff2b34a25a02b6
Soro Franck Albert <sorofranck3@gmail.com>
2026-05-14 20:40:26 +0000

    fix: vercel 013

--- src/build.py
+++ src/build.py
@@ -1,15 +1,18 @@
 import subprocess
+import os
 
 def main():
-    # Compiler Tailwind CSS
+    # Remonter à la racine pour trouver static_src
+    static_src = os.path.join(os.path.dirname(__file__), 'theme', 'static_src')
+    
     subprocess.run(
         ["npm", "install"],
-        cwd="src/theme/static_src",
+        cwd=static_src,
         check=True
     )
     subprocess.run(
         ["npm", "run", "build"],
-        cwd="src/theme/static_src",
+        cwd=static_src,
         check=True
     )
 
