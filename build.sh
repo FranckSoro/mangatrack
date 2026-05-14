8023b6720c09f561f8deabd45101a39a1c4888d1
Soro Franck Albert <sorofranck3@gmail.com>
2026-05-14 16:49:44 +0000

    fix: vercel 004

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ set -x
 
 # Installation des dépendances
 echo "Installation des dépendances"
-python3.12 -m pip install -r src/requirements.txt --break-system-packages
+python3.12 -m pip install -r requirements.txt --break-system-packages
 
 # Collecte des fichiers statiques
 echo "Collection des fichiers statiques"
