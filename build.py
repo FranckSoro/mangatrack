2a36bc7ba086d1bd96e42aed534bdd9cb0aedadf
Soro Franck Albert <sorofranck3@gmail.com>
2026-05-14 21:04:22 +0000

    fix: vercel 016

--- build.py
+++ build.py
@@ -2,8 +2,7 @@ import subprocess
 import os
 
 def main():
-    static_src = os.path.join("src", "theme", "static_src")
-    
+    static_src = os.path.join(os.path.dirname(__file__), 'theme', 'static_src')
     subprocess.run(["npm", "install"], cwd=static_src, check=True)
     subprocess.run(["npm", "run", "build"], cwd=static_src, check=True)
 
